5167b54a8a62b58444d9334353a1630a74ced696
nshah003 <neshah@seas.upenn.edu>
2015-06-27 13:51:24 -0400

    added sketch

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,5 +1,7 @@
 #include <Tobi.h>
 #include <TimerOne.h>
+#include <I2Cdev.h>
+#include <MPU6050.h>
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,4 +12,3 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
-
